4d5a29e00da9aae9050946ef23a896d8b3fe43d0
WaterRedLover0110 <kevinrowland.dev0110@gmail.com>
2024-11-22 23:47:31 -0500

    Fix test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+/// <reference types="vitest/config" />
+
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
